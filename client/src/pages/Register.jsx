e08ade5b72d5ed1ff0f87a5871699a92487cbf04
muginga72 <lmj.muginga@gmail.com>
2025-10-09 16:57:02 -0400

    Bug: fixed the bug related to users sign up, login.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -14,7 +14,7 @@ function Register() {
     e.preventDefault();
 
     try {
-      const res = await fetch("/api/auth/register", {
+      const res = await fetch("/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
