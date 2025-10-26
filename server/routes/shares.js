b57dbccdf945f4df8afa91ab446359673e026714
muginga72 <lmj.muginga@gmail.com>
2025-10-26 17:36:46 -0400

    Bug: the schedule bug fixed.

--- server/routes/shares.js
+++ server/routes/shares.js
@@ -1,25 +1,12 @@
-// routes/shares.js
 const express = require('express');
-const router  = express.Router();
-const ServiceShare = require('../models/ServiceShare');
+const router = express.Router();
+const shareController = require('../controllers/shareController');
 
-// Create
-router.post('/', async (req, res) => {
-  const share = await ServiceShare.create(req.body);
-  // optionally integrate nodemailer here to actually send the email
-  res.json(share);
-});
-
-// Read all
-router.get('/', async (req, res) => {
-  const list = await ServiceShare.find().sort('-createdAt');
-  res.json(list);
-});
-
-// Delete
-router.delete('/:id', async (req, res) => {
-  await ServiceShare.findByIdAndDelete(req.params.id);
-  res.json({ deleted: true });
-});
+// Create; Read; Update; Delete routes for ServiceShare
+router.post('/', shareController.createServiceShare);
+router.get('/', shareController.getAllServiceShares);
+router.get('/:id', shareController.getServiceShareById);
+router.put('/:id', shareController.updateServiceShare);
+router.delete('/:id', shareController.deleteServiceShare);
 
 module.exports = router;
