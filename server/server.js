3b08d62dd6854f2e9396941cdba87b638cb42357
muginga72 <lmj.muginga@gmail.com>
2025-11-30 15:22:48 -0500

    WIP

--- server/server.js
+++ server/server.js
@@ -47,6 +47,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ["Content-Type","Authorization"],
     credentials: true,
   })
 );
